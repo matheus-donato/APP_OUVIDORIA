e1c2ca9da3a44ef2a1be98e167b4e23419b0f54f
matheus-donato <matheus@MPRJ-139116.localdomain>
2020-07-09 18:33:50 -0300

    add files to deploy

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import pandas as pd
 from pandas import ExcelWriter
 from utils import api_classificador
 import json
-import seaborn as sns
 import base64
 from io import BytesIO
 from local_css import local_css
@@ -31,68 +30,69 @@ def get_table_download_link(df):
     b64 = base64.b64encode(val)  # val looks like b'...'
     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="extract.xlsx">Baixar arquivo Excel</a>' # decode b'abc' => abc
 
-
-#Criando barra lateral
-st.sidebar.image("images/logo_azul_horizontal-05.png",use_column_width = True)
-fun_select = st.sidebar.selectbox(
-    "",
-    ("Classificação direta","Classificação a partir de um arquivo"))
-
-#Classificação direta
-
-if fun_select == "Classificação direta":
-
-    st.title("Classificador de temas da ouvidoria")
-
-    texto = st.text_area("Cole o texto da denúncia aqui:")
-
-    if st.button('Classificar'):
-
-        #Chama a API e constroi dataframe com resultados
-        df = api_results_data_frame(texto)
-
-        #Refinando resuldados
-        df = df.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending=False)
-
-        cm = sns.light_palette("green", as_cmap=True)
-        df_format = df.style.format({"Probabilidade": lambda x: "{:.2%}".format(x)}).background_gradient(cmap=cm)
-        #Mostrando o resultado
-        
-        local_css("style.css")
-        def temas_print(temas:list):
+def temas_print(temas:list):
             for tema,cor in zip(temas, range(1,len(temas)+1)):
                 t = f"<span class = 'highlight green{cor}'>{tema}</span>"
                 st.markdown(t, unsafe_allow_html=True)
 
-        temas_print(df.Promotoria.tolist())
+def main():
 
-else:
-    st.title("Outra funcionalidade")
+    #Criando barra lateral
+    st.sidebar.image("images/logo_azul_horizontal-05.png",use_column_width = True)
+    fun_select = st.sidebar.selectbox(
+        "",
+        ("Classificação direta","Classificação a partir de um arquivo"))
 
-    arquivo = st.file_uploader("Anexe um arquivo .xlsx (Planilha Excel)",type="xlsx")
-    coluna_texto = st.text_input("Nome da coluna com os textos")
+    #Classificação direta
 
-    if arquivo is not None:
-        df = pd.read_excel(arquivo).head(8)
-        st.write(df)
+    if fun_select == "Classificação direta":
 
-        if st.button("Classificar"):
-        
-            st.markdown("### Resultados")
+        st.title("Classificador de temas da ouvidoria")
+
+        texto = st.text_area("Cole o texto da denúncia aqui:")
+
+        if st.button('Classificar'):
 
             #Chama a API e constroi dataframe com resultados
-            lista_df = []
-            with st.spinner("Classificando textos..."):
-                for texto in df[coluna_texto]:
-                    r = api_results_data_frame(texto)
-                    r["Texto"] = texto
-                    r = r.sort_values("Probabilidade",ascending = False).head(5)
+            df = api_results_data_frame(texto)
 
-                    st.write(r)
+            #Refinando resuldados
+            df = df.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending=False)
 
-            st.success("Classificação finalizada")
+            #Mostrando o resultado        
+            local_css("style.css")
+            temas_print(df.Promotoria.tolist())
 
-            df_final = r
+    else:
+        st.title("Outra funcionalidade")
 
-            st.write(df_final)
-            st.markdown(get_table_download_link(df_final), unsafe_allow_html=True)
+        arquivo = st.file_uploader("Anexe um arquivo .xlsx (Planilha Excel)",type="xlsx")
+        coluna_texto = st.text_input("Nome da coluna com os textos")
+
+        if arquivo is not None:
+            df = pd.read_excel(arquivo).head(8)
+            st.write(df)
+
+            if st.button("Classificar"):
+            
+                st.markdown("### Resultados")
+
+                #Chama a API e constroi dataframe com resultados
+                lista_df = []
+                with st.spinner("Classificando textos..."):
+                    for texto in df[coluna_texto]:
+                        r = api_results_data_frame(texto)
+                        r["Texto"] = texto
+                        r = r.sort_values("Probabilidade",ascending = False).head(5)
+
+                        st.write(r)
+
+                st.success("Classificação finalizada")
+
+                df_final = r
+
+                st.write(df_final)
+                st.markdown(get_table_download_link(df_final), unsafe_allow_html=True)
+
+if __name__=="__main__":
+    main()
