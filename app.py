644e9c9cd5ef6e0d49e974d579271424aa6aa271
matheus-donato <donats.donato@gmail.com>
2020-08-13 16:41:56 -0300

    add subtemas classification

--- app.py
+++ app.py
@@ -7,29 +7,36 @@ import base64
 from io import BytesIO
 from local_css import local_css
 import time
+import SessionState
 
 def api_results_data_frame(texto):
-    resultado = api_classificador(texto)
+    resultado = api_classificador(texto,endpoint='ouvidoria')
+    resultado_json = json.loads(resultado)
+    df = pd.DataFrame({"Promotoria": resultado_json["temas"],"Probabilidade":resultado_json["p"], "Temas_sub":resultado_json["temas_sub"]})
+    return df
+
+def api_results_data_frame_subtema(texto, tema):
+    resultado = api_classificador(texto = texto,endpoint='ouvidoria/temas',tema=tema)
     resultado_json = json.loads(resultado)
     df = pd.DataFrame({"Promotoria": resultado_json["temas"],"Probabilidade":resultado_json["p"]})
     return df
 
-def to_excel(df):
-    output = BytesIO()
-    writer = ExcelWriter(output,engine="xlsxwriter")
-    df.to_excel(writer)
-    writer.save()
-    processed_data = output.getvalue()
-    return processed_data
+# def to_excel(df):
+#     output = BytesIO()
+#     writer = ExcelWriter(output,engine="xlsxwriter")
+#     df.to_excel(writer)
+#     writer.save()
+#     processed_data = output.getvalue()
+#     return processed_data
 
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    val = to_excel(df)
-    b64 = base64.b64encode(val)
-    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="temas_classificados.xlsx">Baixar arquivo Excel</a>' # decode b'abc' => abc
+# def get_table_download_link(df):
+#     """Generates a link allowing the data in a given panda dataframe to be downloaded
+#     in:  dataframe
+#     out: href string
+#     """
+#     val = to_excel(df)
+#     b64 = base64.b64encode(val)
+#     return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="temas_classificados.xlsx">Baixar arquivo Excel</a>' # decode b'abc' => abc
 
 def temas_print(temas:list):
             for tema,cor in zip(temas, range(1,len(temas)+1)):
@@ -37,13 +44,13 @@ def temas_print(temas:list):
                 st.markdown(t, unsafe_allow_html=True)
 
 def main():
-
     #Criando barra lateral
     st.sidebar.image("images/logo_desenvolvido-01.png",use_column_width = True)
     fun_select = st.sidebar.selectbox(
         "",
-        ("Classificação direta","Classificação a partir de um arquivo"))
+        ["Classificação direta"])
 
+    
     #Classificação direta
     if fun_select == "Classificação direta":
 
@@ -51,8 +58,14 @@ def main():
 
         texto = st.text_area("Cole o texto da denúncia aqui:")
 
-        if st.button('Classificar'):
-
+        session_state = SessionState.get(name="", classificar = False)
+        classificar = st.button('Temas')
+        if texto == '' and classificar is True:
+                st.error("Digite ou cole um texto de ouvidoria.")        
+        if (classificar or session_state.classificar) and texto != "":                
+            st.markdown("---")
+            st.markdown("### Temas preditos")
+            session_state.classificar = True
         #Chama a API e constroi dataframe com resultados
             df = api_results_data_frame(texto)
 
@@ -60,45 +73,69 @@ def main():
             df = df.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending=False)
 
             #Mostrando o resultado        
+            temas_preditos = df['Promotoria'].tolist()
+            temas_selecionados = st.multiselect('', temas_preditos, default=temas_preditos)
+            temas_sub = df.query(f"Promotoria in {temas_preditos}").Temas_sub.tolist()
             local_css("style.css")
-            temas_print(df.Promotoria.tolist())
-    
-    #Classificação a partir de um arquivo
-    else:
-        arquivo = st.file_uploader("Anexe um arquivo .xlsx (Planilha Excel)",type="xlsx")
-        #coluna_texto = st.text_input("Escreva o nome da coluna com os textos e pressione enter.")
-        if arquivo is not None:
-            df = pd.read_excel(arquivo)
-            df.columns.str.title()
-            coluna_texto = df.columns.tolist()[0]
-            st.write(df.head(8))
+            temas_print(temas_selecionados)
 
-            if st.button("Classificar"):
+            #Classificação de subtemas
+        session_state_sub = SessionState.get(name="", classificar_subtemas=False)
+        classificar_subtemas = st.button('Subtemas')
+        if classificar_subtemas:
+            st.markdown('''
+            ---
+            ### Subtemas preditos
+            ''')
+            for idx, tema_s in enumerate(temas_selecionados):
+                    tema = temas_sub[idx]
+                    st.markdown(f"#### {tema_s}")
+                    df = api_results_data_frame_subtema(texto= texto, tema=tema)
+
+                    #Refinando resuldados
+                    df_sub = df.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending=False)
+                    #Mostrando o resultado        
+                    temas_preditos = df_sub['Promotoria'].tolist()
+                    #temas_selecionados = st.multiselect('', temas_preditos, default=temas_preditos)
+                    local_css("style.css")
+                    temas_print(temas_preditos)
             
-                st.markdown("### Resultados")
+    # #Classificação a partir de um arquivo
+    # else:
+    #     arquivo = st.file_uploader("Anexe um arquivo .xlsx (Planilha Excel)",type="xlsx")
+    #     #coluna_texto = st.text_input("Escreva o nome da coluna com os textos e pressione enter.")
+    #     if arquivo is not None:
+    #         df = pd.read_excel(arquivo)
+    #         df.columns.str.title()
+    #         coluna_texto = df.columns.tolist()[0]
+    #         st.write(df.head(8))
 
-                #Chama a API e constroi dataframe com resultados
-                df_final = pd.DataFrame()
-                my_bar = st.progress(0)                
-                n_texto = 0
-                passos = 1/len(df[coluna_texto])
+    #         if st.button("Classificar"):
+            
+    #             st.markdown("### Resultados")
 
-                for texto in df[coluna_texto]:
-                    r = api_results_data_frame(texto)
+    #             #Chama a API e constroi dataframe com resultados
+    #             df_final = pd.DataFrame()
+    #             my_bar = st.progress(0)                
+    #             n_texto = 0
+    #             passos = 1/len(df[coluna_texto])
 
-                    #Refinando resultados
-                    r["Texto"] = texto
-                    r = r.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending = False).reset_index()
-                    r = r.groupby("Texto").agg(Promotorias = ("Promotoria",", ".join)).reset_index()
+    #             for texto in df[coluna_texto]:
+    #                 r = api_results_data_frame(texto)
 
-                    #Atualizando resultado final
-                    df_final = pd.concat([df_final,r],ignore_index=True)
-                    time.sleep(0.5)
-                    n_texto += passos
-                    my_bar.progress(n_texto)
+    #                 #Refinando resultados
+    #                 r["Texto"] = texto
+    #                 r = r.query("Probabilidade >= 0.1").sort_values("Probabilidade",ascending = False).reset_index()
+    #                 r = r.groupby("Texto").agg(Promotorias = ("Promotoria",", ".join)).reset_index()
 
-                #Disponibiliza download da planilha com resultados
-                st.markdown(get_table_download_link(df_final), unsafe_allow_html=True)
+    #                 #Atualizando resultado final
+    #                 df_final = pd.concat([df_final,r],ignore_index=True)
+    #                 time.sleep(0.5)
+    #                 n_texto += passos
+    #                 my_bar.progress(n_texto)
+
+    #             #Disponibiliza download da planilha com resultados
+    #             st.markdown(get_table_download_link(df_final), unsafe_allow_html=True)
 
 #Removendo rodapé com o nome do streamlit
 hide_footer_style = """
--- utils.py
+++ utils.py
@@ -1,13 +1,37 @@
-def api_classificador(texto):
+def api_classificador(texto,endpoint,tema=None):
     import requests
 
-    url = "https://api-ouvidoria.herokuapp.com/classificacao"
+    #url = "https://api-ouvidoria.herokuapp.com/classificacao"
+    url = f"http://127.0.0.1:5000/{endpoint}"
+    payload = {}
+    token = get_token()
+    if 'temas' in endpoint:
 
-    payload = [{'texto': f"{texto}"}]
-    headers = {
-    'Content-Type': 'application/json'
-    }
+        headers = {
+        'texto':texto,
+        'tema':tema,
+        'Authorization': f'JWT {token}'
+        }
+    else:
+        headers = {
+        'texto':texto,
+        'Authorization': f'JWT {token}'
+        }
 
     response = requests.request("POST", url, headers=headers, data = str(payload))
 
     return response.text.encode('utf8')
+
+def get_token():
+    import json
+    import requests
+    url = "http://127.0.0.1:5000/auth"
+
+    payload = {"username": "Inova","password":"inova*mprj1"}
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
+    response = response.json()
+    return response['access_token']
