5a4e63158eeb2e0583bc210e7f52747e6f315595
matheus-donato <48919724+matheus-donato@users.noreply.github.com>
2022-03-16 04:41:13 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,37 @@
-def api_classificador(texto):
+def api_classificador(texto,endpoint,tema=None):
     import requests
 
-    url = "https://api-ouvidoria.herokuapp.com/classificacao"
+    #url = "https://api-ouvidoria.herokuapp.com/classificacao"
+    url = f"https://api-ouvidoria.herokuapp.com/{endpoint}"
+    payload = {}
+    token = get_token()
+    if 'temas' in endpoint:
 
-    payload = [{'texto': f"{texto}"}]
-    headers = {
-    'Content-Type': 'application/json'
-    }
+        headers = {
+        'texto':texto,
+        'tema':tema,
+        'Authorization': f'JWT {token}'
+        }
+    else:
+        headers = {
+        'texto':texto,
+        'Authorization': f'JWT {token}'
+        }
 
     response = requests.request("POST", url, headers=headers, data = str(payload))
 
     return response.text.encode('utf8')
+
+def get_token():
+    import json
+    import requests
+    url = "https://api-ouvidoria.herokuapp.com/auth"
+
+    payload = {"username": "Inova","password":"inova*mprj1"}
+    headers = {
+    'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", url, headers=headers, data = json.dumps(payload))
+    response = response.json()
+    return response['access_token']
